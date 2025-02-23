821d7c08248e93db18d99845b7b875c51e98a4a8
muhammad <muhammadalawiytahar@gmail.com>
2025-02-24 00:38:07 +0700

    Update PhantasmRewrite.lua

--- PhantasmRewrite.lua
+++ PhantasmRewrite.lua
@@ -1,1 +1,6 @@
 loadstring(game:HttpGet("https://raw.githubusercontent.com/Muhammad39843209/PhantasmRewrite/main/PhantasmRewrite.lua"))()
+print("PhantasmRewrite script loaded!") -- Ini script utama
+
+-- Tambahkan kode cheat di bawah sini
+game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = 100 -- Cheat speed hack
+game.Players.LocalPlayer.Character.Humanoid.JumpPower = 150 -- Cheat jump power
